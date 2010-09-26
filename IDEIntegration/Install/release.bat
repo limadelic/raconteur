9f9d463fc4a4f967d715ddc7a534b8229fbde80e
msuarz <mikemps@gmail.com>
2010-09-26 18:43:20 -0400

    improving release.bat

--- IDEIntegration/Install/release.bat
+++ IDEIntegration/Install/release.bat
@@ -1,4 +1,10 @@
 copy ..\bin\Debug\IDEIntegration\*.* ..\..\live /y
 copy ..\bin\Debug\ItemTemplates\Templates.zip ..\..\live\RaconteurFeature.zip /y
 
+if "%programfiles(x86)%"=="" (
+  copy ..\bin\Debug\IDEIntegration\*.* "%programfiles%\Raconteur" /y
+) else (
+  copy ..\bin\Debug\IDEIntegration\*.* "%programfiles(x86)%\Raconteur" /y
+)
+
 pause

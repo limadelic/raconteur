e06aaa3cb8e87284ce40a575aa01afb81679b070
msuarz <mikemps@gmail.com>
2010-12-05 13:00:58 -0500

    indexed Settings by Projects

--- Raconteur/Helpers/Settings.cs
+++ Raconteur/Helpers/Settings.cs
@@ -1,9 +1,50 @@
+using System.Collections.Generic;
+
 namespace Raconteur
 {
+    public class Setting
+    {
+        public XUnit XUnit = XUnits.Framework["mstest"];
+
+        public Language Language = Languages.In["en"];
+    }
+
     public static class Settings
     {
-        public static XUnit XUnit = XUnits.Framework["mstest"];
+        static Settings() { Project = DefaultProject; }
 
-        public static Language Language = Languages.In["en"];
+        static readonly object DefaultProject = new object();
+        public static void RestoreDefaults() { project = DefaultProject; }
+
+        static object project;
+        public static object Project
+        {
+            get { return project; }
+            set
+            {
+                if (value == null) value = DefaultProject;
+
+                if (!SettingFor.ContainsKey(value)) 
+                    SettingFor[value] = new Setting();
+
+                project = value;
+            }
+        }
+
+        static readonly Dictionary<object, Setting> SettingFor = new Dictionary<object, Setting>();
+
+        static Setting Setting { get { return SettingFor[project]; } }
+
+        public static XUnit XUnit
+        {
+            get { return Setting.XUnit; } 
+            set { Setting.XUnit = value; } 
+        }
+
+        public static Language Language
+        {
+            get { return Setting.Language; } 
+            set { Setting.Language = value; } 
+        }
     }
 }

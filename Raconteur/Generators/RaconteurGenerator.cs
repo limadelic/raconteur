a138f30e6c40e931efc6e0c82ca27b80ba10e0ba
msuarz <mikemps@gmail.com>
2011-04-17 23:34:32 -0400

    append generation error to runner instead of override with error

--- Raconteur/Generators/RaconteurGenerator.cs
+++ Raconteur/Generators/RaconteurGenerator.cs
@@ -1,4 +1,6 @@
-﻿using Raconteur.Compilers;
+﻿using System;
+using System.IO;
+using Raconteur.Compilers;
 using Raconteur.Helpers;
 using Raconteur.IDE;
 using Raconteur.Parsers;
@@ -20,17 +22,38 @@ namespace Raconteur.Generators
 
         public string Generate(string FeatureFilePath, string Content)
         {
+            try
+            {
+                var Feature = Parse(FeatureFilePath, Content);
+
+                GenerateStepDefinitions(Feature);
+
+                FeatureCompiler.Compile(Feature, FeatureItem);
+
+                return ObjectFactory.NewRunnerGenerator(Feature).Code;
+            } 
+            catch (Exception e)
+            {
+                return AppendErrorToOriginalRunner(FeatureFilePath, e);
+            }
+        }
+
+        Feature Parse(string FeatureFilePath, string Content) 
+        {
             Project.LoadFrom(FeatureItem);
 
             var FeatureFile = new FeatureFile(FeatureFilePath){Content = Content};
 
-            var Feature = FeatureParser.FeatureFrom(FeatureFile, FeatureItem);
+            return FeatureParser.FeatureFrom(FeatureFile, FeatureItem);
+        }
 
-            FeatureCompiler.Compile(Feature, FeatureItem);
+        string AppendErrorToOriginalRunner(string FeatureFilePath, Exception e) 
+        {
+            var RunnerFile = FeatureFilePath.Replace(".feature", ".runner.cs");
+            var OriginalRunner = File.Exists(RunnerFile) ? File.ReadAllText(RunnerFile) : string.Empty;
+            var eol = Environment.NewLine;
 
-            GenerateStepDefinitions(Feature);
-
-            return ObjectFactory.NewRunnerGenerator(Feature).Code;
+            return "/*" + eol + e + eol + "*/" + eol + OriginalRunner;
         }
 
         // Tech Debt: this is side effect to generate

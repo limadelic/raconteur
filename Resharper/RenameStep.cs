bd939678a99951d8e99bfbb77691462938a37019
msuarz <mikemps@gmail.com>
2011-06-17 22:23:39 -0400

    done spiking calling Rename Method

--- Resharper/RenameStep.cs
+++ Resharper/RenameStep.cs
@@ -1,37 +1,55 @@
 ﻿using System;
+using JetBrains.ActionManagement;
+using JetBrains.Application;
+using JetBrains.Application.DataContext;
 using JetBrains.Application.Progress;
+using JetBrains.DataFlow;
 using JetBrains.ProjectModel;
 using JetBrains.ReSharper.Feature.Services.Bulbs;
 using JetBrains.ReSharper.Feature.Services.CSharp.Bulbs;
-using JetBrains.ReSharper.Intentions;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.Services;
+using JetBrains.ReSharper.Refactorings.Rename;
 using JetBrains.TextControl;
 using JetBrains.Util;
 
 namespace Raconteur.Resharper
 {
-    public abstract class CSharpOneItemContextAction : BulbItemImpl, IContextAction
-    {
-        protected readonly ICSharpContextActionDataProvider Provider;
-
-        protected CSharpOneItemContextAction(ICSharpContextActionDataProvider Provider) { this.Provider = Provider; }
-
-        public abstract bool IsAvailable(IUserDataHolder cache);
-
-        public new IBulbItem[] Items { get { return new[] {this}; } }
-    }
-
     [ContextAction(Group = "C#", Name = "Rename Step", Description = "do it", Priority = 15)]
-    public class RenameStep : CSharpOneItemContextAction
+    public class RenameStep : SingleItemContextAction
     {
-        public RenameStep(ICSharpContextActionDataProvider provider) : base(provider) { }
+        public RenameStep(ICSharpContextActionDataProvider Provider) : base(Provider) { }
 
         public override string Text { get { return "Rename Step"; } }
 
-        public override bool IsAvailable(IUserDataHolder cache) { return true; }
+        IMethodDeclaration Method;
 
-        protected override Action<ITextControl> ExecutePsiTransaction(ISolution solution, IProgressIndicator progress)
+        public override bool IsAvailable(IUserDataHolder Cache)
         {
-            MessageBox.ShowInfo("getting there");
+            Method = Provider.GetSelectedElement<IMethodDeclaration>(false, true);
+ 
+            return Method != null;
+        }
+
+        protected override Action<ITextControl> ExecutePsiTransaction(ISolution Solution, IProgressIndicator Progress)
+        {
+            try
+            {
+                var Rules = DataRules
+                    .AddRule("ManualRenameRefactoringItem", JetBrains.ReSharper.Psi.Services.DataConstants.DECLARED_ELEMENTS, Method.DeclaredElement.ToDeclaredElementsDataConstant())
+                    .AddRule("ManualRenameRefactoringItem", JetBrains.TextControl.DataContext.DataConstants.TEXT_CONTROL, Provider.TextControl)
+                    .AddRule("ManualRenameRefactoringItem", JetBrains.ProjectModel.DataContext.DataConstants.SOLUTION, Solution);
+
+                Lifetimes.Using(Lifetime => 
+                    RenameRefactoringService.ExcuteRename(
+                        Shell.Instance.Components.ActionManager()
+                            .DataContexts.Create(Lifetime, Rules)));
+            } 
+            catch (Exception e)
+            {
+                MessageBox.ShowInfo(e.ToString());
+            }
+
             return null;
         }
     }
